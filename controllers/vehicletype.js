ba96414d58ff5038291d5850a4de29c1f7de2544
MaximilianGeorgiev <scissorhands.tw@gmail.com>
2022-05-30 16:40:45 +0300

    chore: Add basic structure for express routing

---
+++ controllers/vehicletype.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+const VehicleTypeController = require("../controllers/vehicletype.js");
+
+router.get("/:id", () => {});
+router.get("/:type", () => {});
+
+module.exports = router;
