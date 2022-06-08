10aad815cbff79d0e8cc87a98ab9903c13f6f9e7
MaximilianGeorgiev <scissorhands.tw@gmail.com>
2022-06-08 13:19:07 +0300

    feat: Add blogs CRUD functionality

--- routes/blogpost.js
+++ routes/blogpost.js
@@ -3,6 +3,20 @@ const router = express.Router();
 
 const BlogController = require("../controllers/blogpost.js");
 
-router.get("/:id", () => {});
+router.route('/')
+.get(BlogController.getAllBlogs)
+.post(BlogController.createBlog);
+
+router.get('/id/:id', ServiceController.getBlogById);
+router.get('/title/:title', ServiceController.getBlogByTitle);
+router.get('/author/:id', ServiceController.getBlogByAuthorId);
+router.get('/delete/:id', ServiceController.deleteBlog); 
+
+router.get('/pictures/:id', ServiceController.getBlogPictures);
+
+router.post('/picture/add', ServiceController.addPicture);
+router.post('/picture/remove', ServiceController.removePicture);
+
+router.put('/:id', ServiceController.updateBlog);
 
 module.exports = router;
