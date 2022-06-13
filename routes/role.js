937f69761eba6a6d92c0231094773a0cf4ba7ee0
MaximilianGeorgiev <scissorhands.tw@gmail.com>
2022-06-13 13:38:32 +0300

    fix: Get role headers not required

--- routes/role.js
+++ routes/role.js
@@ -7,8 +7,8 @@ const AuthUtils = require("../utils/auth.js");
 router.get('/', RoleController.getAllRoles);
 router.post('/', AuthUtils.validateToken, RoleController.createRole);
 
-router.get('/id/:id', AuthUtils.validateToken, RoleController.getRoleById);
-router.get('/name/:name', AuthUtils.validateToken, RoleController.getRoleByName);
+router.get('/id/:id', RoleController.getRoleById);
+router.get('/name/:name', RoleController.getRoleByName);
 router.get('/delete/:id', AuthUtils.validateToken, RoleController.deleteRole); 
 
 router.put('/:id', AuthUtils.validateToken, RoleController.updateRole);
