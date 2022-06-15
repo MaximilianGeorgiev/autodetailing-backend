8d8f640b815972ef7bb6342251001097d30b8018
MaximilianGeorgiev <scissorhands.tw@gmail.com>
2022-06-15 12:51:29 +0300

    feat: Finish picture upload functionality

--- controllers/picture.js
+++ controllers/picture.js
@@ -1,3 +1,11 @@
+var path = require("path");
+
 exports.addPicture = (request, response) => {
-    console.log("test" + JSON.stringify(request));
+    let paths = [];
+    
+    // file paths are sent so they can be used and inserted into "EntityPicture" table
+    for (let i = 0; i < request.files.length; i++)
+        paths.push(path.resolve(request.files[i].path));
+
+    response.status(200).json({ status: "success", paths: paths });
 };
--- routes/picture.js
+++ routes/picture.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const multer = require("multer");
+const path = require('path');
 
-const upload = multer({ dest: "uploads/" });
+var storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, './uploads');
+     },
+    });
 
+const upload = multer({ storage: storage });
 
 const PictureController = require('../controllers/picture.js');
 
